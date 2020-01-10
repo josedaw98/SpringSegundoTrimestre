2ba125574977e51329dbc7918f7fa827ec703ddb
JoseRamon <juniorkollanzo@gmail.com>
2020-01-10 12:06:36 +0100

    autor con getters y setters

--- inversionOfControl/src/main/java/com/jose/IOC/beans/Autor/AutorBean.java
+++ inversionOfControl/src/main/java/com/jose/IOC/beans/Autor/AutorBean.java
@@ -2,4 +2,23 @@ package com.jose.IOC.beans.Autor;
 
 public class AutorBean {
 
+	
+	 private String nombre;
+	 private int edad;
+	 
+	 
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	 
+	 
 }
--- inversionOfControl/src/main/java/com/jose/IOC/beans/GeneradorAutores.java
+++ inversionOfControl/src/main/java/com/jose/IOC/beans/GeneradorAutores.java
@@ -1,6 +1,13 @@
 package com.jose.IOC.beans;
 
+import com.jose.IOC.beans.Autor.AutorBean;
+
 public class GeneradorAutores {
 
+	
+	public AutorBean juan() {
+		AutorBean autor =  new AutorBean();
+		autor.
+	}
 	
 }
